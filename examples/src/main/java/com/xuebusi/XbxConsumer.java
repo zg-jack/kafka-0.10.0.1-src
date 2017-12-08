d0a7dbabe6e4e7b20fb50d8d3c157c1f772b5e9e
xuebusi <xbs1019@126.com>
2017-12-08 21:48:32 +0800

    测试消费者

--- examples/src/main/java/com/xuebusi/XbxConsumer.java
+++ examples/src/main/java/com/xuebusi/XbxConsumer.java
@@ -4,7 +4,9 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +23,7 @@ public class XbxConsumer {
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        //props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
@@ -29,12 +31,21 @@ public class XbxConsumer {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-xbs-test");
 
         KafkaConsumer consumer = new KafkaConsumer(props);
-        while (true) {
+        /*while (true) {
             consumer.subscribe(Collections.singletonList("zp"));
             ConsumerRecords<String, String> records = consumer.poll(1000);
             for (ConsumerRecord record : records) {
                 System.out.println("===========消费消息====" + record.toString() + "==========");
             }
+        }*/
+
+        TopicPartition partition = new TopicPartition("zp", 0);
+        //手动指定消费位置，无法自动负载均衡，所以要手动注册，才能消费
+        consumer.assign(Arrays.asList(partition));
+        consumer.seek(partition, 531);
+        ConsumerRecords<String, String> records = consumer.poll(1000);
+        for (ConsumerRecord record : records) {
+            System.out.println("===========消费消息====" + record.toString() + "==========");
         }
 
     }
